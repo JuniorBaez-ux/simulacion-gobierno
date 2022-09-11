d000f59845331473ea45e2df3959da42559d9e0b
Junior Baez <junior_baez@ucne.edu.do>
2022-09-11 17:36:54 -0400

    Implementando primera parte de la simulacion de gobernanza

---
+++ src/pueblo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Pueblo(props){
+    
+      return (
+        <div>
+            <h1>Pueblo</h1>
+            <h2>Partido: {props.contienda}</h2>
+        </div>
+      );
+
+}
+
+export default Pueblo;
