94c3d4940f05378eaccc467a580e5a91b1fca08e
Junior Baez <junior_baez@ucne.edu.do>
2022-09-12 20:56:29 -0400

    Mejorando diseño de la simulacion

--- src/gobierno.js
+++ src/gobierno.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Gobierno(props){
 
       return (
-        <div>
-            <h1>Gobierno</h1>
-            <h2>Partido: {props.partido}</h2>
-            <h2>Politica: {props.politica}</h2>
+        <div class="pt-4">
+            <h1 class="text-2xl font-bold text-center text-gray-800">Gobierno</h1>
+            <h2 class="text-2md font-bold text-center text-gray-800">Partido: {props.partido}</h2>
+            <h2 class="text-2md font-bold text-center text-gray-800">Politica: {props.politica}</h2>
         </div>
       );
 }
--- src/pueblo.js
+++ src/pueblo.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Pueblo(props){
     
       return (
-        <div>
-            <h1>Pueblo</h1>
-            <h2>Partido: {props.contienda}</h2>
+        <div class="pt-4">
+            <h1 class="text-2xl font-bold text-center text-gray-800">Pueblo</h1>
+            <h2 class="text-2md font-bold text-center text-gray-800">Nivel de contienda: {props.contienda}</h2>
         </div>
       );
 
